a2a46abd780c9e5e00fbd23f302c053360ce772b
Christian Eder <christian.eder@zuehlke.com>
2019-05-15 08:23:15 +0200

    add sample on how to override default resource rendering

---
+++ IotReferenceArchitectureFunctions/IotReferenceArchitectureFunctions.Architecture/Renderer/IotHubRenderer.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace IotReferenceArchitectureFunctions.Architecture.Renderer
+{
+    public class IoTHubRenderer : Structurizr.InfrastructureAsCode.Azure.ARM.IoTHubRenderer
+    {
+        protected override JObject PostProcess(JObject template)
+        {
+            template = base.PostProcess(template);
+            template["sku"]["name"] = "S1";
+            return template;
+        }
+    }
+}
